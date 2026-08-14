69fb5bf6e691627e05cfd2e225c93329a02d1234
root <root@github-action.asia-southeast1-c.c.devops-projectest.internal>
2026-08-14 16:22:05 +0000

    update app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello! Aplikasi ini dideploy otomatis lewat GitHub Actions & Docker!\n');
+  res.end('Hello! Aplikasi ini dideploy otomatis lewat GitHub Actions & Docker! saya berhasil\n');
 });
 
 server.listen(port, () => {
