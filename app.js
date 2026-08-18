e896350c2ee3f225f9e625abc45726026b18a6ca
AMD-id <halloambas@gmail.com>
2026-08-18 16:42:45 +0700

    Change response message to 'Hallo World'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('saya ganti baru\n');
+  res.end('Hallo World\n');
 });
 
 server.listen(port, () => {
